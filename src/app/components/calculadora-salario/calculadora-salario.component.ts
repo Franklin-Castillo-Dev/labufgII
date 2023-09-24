631c4cb88d339fc57987802a15f91dd2efe8a6ff
Franklin <fran.developer.2022@gmail.com>
2023-09-23 20:12:41 -0600

    creacion de funcion que opera isss y afp

--- src/app/components/calculadora-salario/calculadora-salario.component.ts
+++ src/app/components/calculadora-salario/calculadora-salario.component.ts
@@ -15,5 +15,23 @@ export class CalculadoraSalarioComponent {
     renta: '0.00',
     liquido: '0.00'
   }
-  
+
+  calcularSalarioLiquido(){
+    console.log(this.datos);
+
+    if(this.datos.salario > 0){
+      this.datos.isss = (this.datos.salario * 0.03).toFixed(2);
+      if(parseFloat(this.datos.isss) > 30){
+        this.datos.isss = '30.00';
+      }
+      this.datos.afp = (this.datos.salario * 0.0725).toFixed(2);
+
+      //Condicionales Renta
+      this.datos.renta = '0.00';
+
+      this.datos.liquido = (this.datos.salario - (+this.datos.isss + +this.datos.afp + +this.datos.renta)).toFixed(2);
+    }
+
+  }
+
 }
