578ade837b9bbebe0fec5f2dc28db4dbb99bc2e2
Emilio <emilioderas3@gmail.com>
2023-09-23 19:14:20 -0600

    Creacion de componente de Calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraSalarioComponent } from './components/calculadora-salario/calculadora-salario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraSalarioComponent
   ],
   imports: [
     BrowserModule,
